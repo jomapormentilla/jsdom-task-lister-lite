7b09e85ea7cf75c49ba456c9d974867126a0fb39
jomapormentilla <jomapormentilla@gmail.com>
2021-01-26 19:01:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form#create-task-form").addEventListener("submit", function(e){
+    e.preventDefault()
+    const li = document.createElement("li")
+    li.innerHTML = document.getElementById("new-task-description").value
+    
+    const close = document.createElement("button")
+    close.id = li.innerHTML
+    close.innerHTML = "X"
+    li.append(close)
+  
+    const ul = document.getElementById("tasks")
+    ul.append(li)
+  })
 });
+
