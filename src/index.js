e01ad7a84337c229bcb20e3c6cc38419f0de7b43
jomapormentilla <jomapormentilla@gmail.com>
2021-01-28 17:38:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     li.innerHTML = document.getElementById("new-task-description").value
     
     const close = document.createElement("button")
-    close.id = li.innerHTML
     close.innerHTML = "X"
     li.append(close)
+    close.addEventListener("click", deleteTask)
+    function deleteTask(e){
+      li.remove()
+    }
   
     const ul = document.getElementById("tasks")
     ul.append(li)
+
+    const textInput = document.getElementById("new-task-description")
+    textInput.value = ""
+    textInput.focus()
   })
 });
 
